44eb6de5a5bab6b238720d3010c241a0860805d2
Lloyd Boone <lboone@progressive.com>
2024-05-14 18:58:42 -0400

    implement uploadthing and interface for creating servers.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
+
+const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/api/uploadthing',
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (!auth().userId && !isProtectedRoute(req)) {
+  if (!auth().userId && !isPublicRoute(req)) {
 
     // Add custom logic to run before redirecting
 
