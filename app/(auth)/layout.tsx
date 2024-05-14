df885e5beba70807c174385ce14378ecfd5c99b8
Lloyd Boone <lboone@progressive.com>
2024-05-14 14:09:17 -0400

    implement clerk auth and sign in/up forms

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-        <h1>Auth Layout</h1>
+    <div className="h-full flex items-center justify-center">
         {children}
     </div>
   )
