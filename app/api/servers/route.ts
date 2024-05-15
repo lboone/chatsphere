13962865477d963115edc2784844748367f853eb
Lloyd Boone <lboone@progressive.com>
2024-05-14 20:54:21 -0400

    implement creating server for current user

---
+++ app/api/servers/route.ts
@@ -0,0 +1,40 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { MemberRole } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+export async function POST(req: Request) {
+    try {
+        const { name, imageUrl } = await req.json();
+        const profile = await currentProfile();
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const server = await db.server.create({
+            data: {
+                name,
+                imageUrl,
+                profileId: profile.id,
+                inviteCode: uuidv4(),
+                channels: {
+                    create: [
+                        {name: "general", profileId: profile.id},
+                    ]
+                },
+                members: {
+                    create: [
+                        {profileId: profile.id, role: MemberRole.ADMIN},
+                    ]
+                }
+            },
+        });
+
+        return new NextResponse(JSON.stringify(server), { status: 200 });
+
+    } catch (error) {
+        console.log("[SERVERS_POST] Error: ", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
