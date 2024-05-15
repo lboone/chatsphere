a3da95069acb4de85611082956cdfee48b2fae3a
Lloyd Boone <lboone@progressive.com>
2024-05-14 22:22:07 -0400

    implement add server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import ModalProvider from "@/components/providers/modal-provider";
+
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -26,6 +28,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="chatsphere-theme"
           >
+            <ModalProvider/>
             {children}
           </ThemeProvider>
         </body>
