b2e7282e850193936d515873d8c43c7a2bc7a491
Lloyd Boone <lboone@progressive.com>
2024-05-16 19:00:06 -0400

    implement role identificatoin in server header menu

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -15,14 +15,25 @@ import {
   PlusCircle,
   Trash,
   LogOut,
+  ShieldAlert,
+  ShieldCheck,
+  Shield,
 } from "lucide-react";
 import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
 import { useModal } from "@/hooks/use-modal-store";
+import ActionTooltip from "@/components/action-tooltip";
+import { cn } from "@/lib/utils";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
   role?: MemberRole;
 }
+const roleIconMap = {
+  ADMIN: <ShieldAlert className="h-4 w-4 ml-auto text-rose-300" />,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-auto text-indigo-300" />,
+  GUEST: <Shield className="h-4 w-4 ml-auto text-zinc-300" />,
+};
+
 const ServerHeader = ({ server, role }: ServerHeaderProps) => {
   const { onOpen } = useModal();
 
@@ -38,40 +49,62 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px] bg-zinc-200 dark:bg-zinc-900">
+        <DropdownMenuItem className={cn("px-3 py-2 text-sm", isAdmin ? "text-rose-300" : isModerator ? "text-indigo-300" : "text-zinc-300")}>
+        {role || "GUEST"}
+        {roleIconMap[role || "GUEST"]}
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
         {isModerator && (
-          <DropdownMenuItem onClick={() => onOpen("invite", { server })} className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+          >
             Invite People
             <UserPlus className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem onClick={() => onOpen("editServer", { server })} className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("editServer", { server })}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
             Server Settings
             <Settings className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem onClick={() => onOpen("members", { server })} className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("members", { server })}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
             Manage Members
             <Users className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem onClick={() => onOpen("createChannel")} className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("createChannel")}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem onClick={() => onOpen("deleteServer", { server })} className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
             Delete Server
             <Trash className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem onClick={() => onOpen("leaveServer", { server })}
-          className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
             Leave Server
             <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
