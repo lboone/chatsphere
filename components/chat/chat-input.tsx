6c12734d214db75cc0cb9676b49e422a925cbfe7
Lloyd Boone <lboone@progressive.com>
2024-05-17 18:07:31 -0400

    implement file upload for channel chat.

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -5,6 +5,7 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import qs from "query-string";
+import { useModal } from "@/hooks/use-modal-store";
 
 import {
     Form,
@@ -36,7 +37,7 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
             content: "",
         }
     });
-
+    const { onOpen } = useModal();
     const isLoading = form.formState.isSubmitting;
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
@@ -60,7 +61,7 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     <FormItem>
                         <FormControl>
                             <div className="relative p-4 pb-6">
-                                <button type="button" onClick={() => {}} className="absolute top-7 left-8 h-[24px] w-[24px] rounded-full bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition  p-1 flex items-center justify-center">
+                                <button type="button" onClick={() => onOpen("messageFile",{apiUrl, query})} className="absolute top-7 left-8 h-[24px] w-[24px] rounded-full bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition  p-1 flex items-center justify-center">
                                     <Plus className="text-white dark:text-[#313338]" />
                                 </button>
                                 <Input
