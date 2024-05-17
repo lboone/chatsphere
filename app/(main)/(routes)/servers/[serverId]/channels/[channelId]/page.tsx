f4e6d416074424a904021e12bb12bf7d054657d3
Lloyd Boone <lboone@progressive.com>
2024-05-17 16:50:35 -0400

    implement chat input for channels

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,10 @@
-import ChatHeader from '@/components/chat/chat-header';
-import { currentProfile } from '@/lib/current-profile';
-import { db } from '@/lib/db';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-import React from 'react'
+import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
 
 interface ChannelIdPageProps {
   params: { serverId: string; channelId: string };
@@ -30,14 +31,25 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   });
 
   if (!channel || !member) {
-    return redirect("/")
+    return redirect("/");
   }
 
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-      <ChatHeader name={channel.name} serverId={params.serverId} type="channel"  />
+      <ChatHeader
+        name={channel.name}
+        serverId={params.serverId}
+        type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{ channelId: channel.id, serverId: channel.serverId }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ChannelIdPage
+export default ChannelIdPage;
