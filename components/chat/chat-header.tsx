6d5990bb9f1cb66ab1c7fe9fc106d949314ea30c
Lloyd Boone <lboone@progressive.com>
2024-05-17 15:21:17 -0400

    implement member conversation interface and database schema.

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Hash } from "lucide-react";
 import MobileToggle from '../mobile-toggle';
+import UserAvatar from '../user-avatar';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -14,6 +15,7 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 ">
         <MobileToggle serverId={serverId} />
         {type === "channel" && ( <Hash className="text-zinc-500 dark:text-zinc-400 mr-2 h-5 w-5" />)}
+        {type === "conversation" && ( <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />)}
         <p className="font-semibold text-md text-black dark:text-white">
             {name}
         </p>
