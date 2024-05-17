d059b61fc14e1335085f4ebeeeaa3e7794f708ec
Lloyd Boone <lboone@progressive.com>
2024-05-17 15:56:11 -0400

    implement connection to socket.io

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Hash } from "lucide-react";
 import MobileToggle from '../mobile-toggle';
 import UserAvatar from '../user-avatar';
+import { SocketIndicator } from '../socket-indicator';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -19,6 +20,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         <p className="font-semibold text-md text-black dark:text-white">
             {name}
         </p>
+        <div className="ml-auto flex items-center">
+            <SocketIndicator />
+        </div>
     </div>
   )
 }
