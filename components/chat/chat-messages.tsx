e68191cb1beb6efc2e93149c408278e0774feadd
Lloyd Boone <lboone@progressive.com>
2024-05-18 23:36:43 -0400

    implement sockets and live messaging in channel chat screen.

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import ChatItem from "./chat-item";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
@@ -36,6 +37,9 @@ const ChatMessages = ({
   type,
 }: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
+
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
       queryKey,
@@ -43,6 +47,7 @@ const ChatMessages = ({
       paramKey,
       paramValue,
     });
+    useChatSocket({ queryKey, addKey, updateKey });
 
   console.log({ data, fetchNextPage, hasNextPage, isFetchingNextPage, status });
   if (status === "pending") {
